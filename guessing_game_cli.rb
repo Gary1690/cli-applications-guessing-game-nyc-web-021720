72b370e390175bcddd3e86eb9a9670700a2c9134
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-22 03:34:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,8 @@ require 'pry'
 def run_guessing_game
   random_number = rand(6)+1
 
-  user_input = (gets.chomp).to_i
-  if(user_input == random_number)
+  user_input = gets.chomp
+  if(user_input.to_i == random_number)
     puts "You guessed the correct number!"
   elsif(user_input == 'exit')
     "GoodBye!"
