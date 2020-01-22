832250349604e272a78583b1212ed2ca128c0948
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-22 03:29:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   random_number = rand(6)+1
-  puts ""
+  puts "Pick a number 1-6"
   user_input = (gets.chomp).to_i
   if(user_input == random_number)
     puts "You guessed the correct number!"
